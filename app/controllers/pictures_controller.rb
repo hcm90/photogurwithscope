331002b00b5ddc850c73113e2f9d7dee789fccf1
“hcm90” <heitor.moura@gmail.com>
2014-01-31 15:34:50 -0500

    Added scopes to index page

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,68 @@
+# class PicturesController < ApplicationController
+
+
+
+#   def new
+#   end
+
+#   def create
+#     render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+#   end
+
+# end
+
+class PicturesController < ApplicationController
+
+   def index
+     @pictures = Picture.all
+     @most_recent_pictures = Picture.most_recent_five
+     @newest_pictures = Picture.newest_first
+     @posted_before = Picture.created_before(1.month.ago)
+   end
+
+   def show
+     @picture = Picture.find(params[:id])
+   end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update_attributes(picture_params)
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+  def create
+    # make a new picture with what picture_params returns (which is a method we're calling)
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # if the save for the picture was successful, go to index.html.erb
+      redirect_to pictures_url
+    else
+      # otherwise render the view associated with the action :new (i.e. new.html.erb)
+      render :new
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_url
+  end
+
+  private
+  def picture_params
+    params.require(:picture).permit(:artist, :title, :url)
+  end
+
+end
